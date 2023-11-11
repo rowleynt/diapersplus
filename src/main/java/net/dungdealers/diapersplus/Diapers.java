407b1cf8f58bee73cf57ba615999b59420a0289c
biglarge <rowleynt@gmail.com>
2023-11-11 00:28:04 -0600

    fuck you

---
+++ src/main/java/net/dungdealers/diapersplus/Diapers.java
@@ -0,0 +1,16 @@
+package net.dungdealers.diapersplus;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Diapers implements ModInitializer {
+	public static final String MOD_ID = "diapersplus";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("poop");
+	}
+}
