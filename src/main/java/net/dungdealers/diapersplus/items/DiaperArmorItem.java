6f5d8026708c2c3f4790792835fc36ad5e4bb32e
biglarge <rowleynt@gmail.com>
2023-11-11 23:30:48 -0600

    added empty DiaperArmorItem class

---
+++ src/main/java/net/dungdealers/diapersplus/items/DiaperArmorItem.java
@@ -0,0 +1,11 @@
+package net.dungdealers.diapersplus.items;
+
+import net.minecraft.item.ArmorItem;
+import net.minecraft.item.ArmorMaterial;
+
+public class DiaperArmorItem extends ArmorItem {
+    public DiaperArmorItem(ArmorMaterial material, Type type, Settings settings) {
+        super(material, type, settings);
+    }
+
+}
