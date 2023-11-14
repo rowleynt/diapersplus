5bdb8e93ea053c6b567bd0304da5215920d8a0ff
biglarge <rowleynt@gmail.com>
2023-11-13 18:50:34 -0600

    not telling :)

--- src/main/java/net/dungdealers/diapersplus/datagen/DPBlockTagProvider.java
+++ src/main/java/net/dungdealers/diapersplus/datagen/DPBlockTagProvider.java
@@ -4,7 +4,6 @@ import net.dungdealers.diapersplus.util.DPTags;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
 import net.minecraft.block.Blocks;
-import net.minecraft.item.Items;
 import net.minecraft.registry.RegistryWrapper;
 
 import java.util.concurrent.CompletableFuture;
