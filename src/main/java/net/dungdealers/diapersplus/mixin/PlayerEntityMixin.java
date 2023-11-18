8a777307416ab46a03fcf338155c2cd9a1a539dc
biglarge <rowleynt@gmail.com>
2023-11-18 09:27:48 -0600

    added injection into tick method + some helper methods to randomly apply status effect not quite working as intended yet

--- src/main/java/net/dungdealers/diapersplus/mixin/PlayerEntityMixin.java
+++ src/main/java/net/dungdealers/diapersplus/mixin/PlayerEntityMixin.java
@@ -3,12 +3,21 @@ package net.dungdealers.diapersplus.mixin;
 import net.dungdealers.diapersplus.DPStatusEffects.DPStatusEffects;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
-public abstract class PlayerEntityMixin extends LivingEntity {
+public abstract class PlayerEntityMixin extends LivingEntity implements Random {
+    @Shadow public abstract boolean isSpectator();
+
+    @Shadow public abstract boolean isCreative();
+
     protected PlayerEntityMixin(EntityType<? extends LivingEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -21,8 +30,55 @@ public abstract class PlayerEntityMixin extends LivingEntity {
      * @see some bitches
      */
     @Unique
+    Random rand = Random.create();
+    @Unique
+    int randint;
+    @Unique
+    int tickCount = 0;
+    @Final
+    @Unique
+    private final int PISSING_DURATION = 200;
+    @Final
+    @Unique
+    private final int MAX_RANDOM_NUMBER = 1000000;
+    @Unique
+    StatusEffectInstance pissingStatus =
+        new StatusEffectInstance(
+            DPStatusEffects.PISSING,
+            PISSING_DURATION,
+            0,
+            false,
+            true
+        );
+
+    /**
+     * @author big large
+     *
+     * Returns a boolean representing whether the player currently has the
+     * pissing status.
+     *
+     * @return boolean for pissing status
+     * @see a therapist
+     */
+    @Unique
     private boolean isPissing() {
         return this.hasStatusEffect(DPStatusEffects.PISSING);
+    }
+
+    /**
+     * @author big large
+     *
+     * Generates a random number between 0 and the value of MAX_RANDOM_NUMBER. If the random
+     * number is equal to 69 (the sex number), and the player is not currently pissing, then
+     * apply the pissing status effect.
+     *
+     * @see you later alligator
+     */
+    @Unique
+    private void tryAddPissingStatus() {
+        randint = rand.nextBetween(0, MAX_RANDOM_NUMBER);
+        if(randint == 69 && !this.isPissing() && (!this.isSpectator() && !this.isCreative()))
+            this.addStatusEffect(pissingStatus);
     }
 
     /**
@@ -39,5 +95,10 @@ public abstract class PlayerEntityMixin extends LivingEntity {
     @Overwrite
     public boolean isImmobile() {
         return super.isImmobile() || this.isSleeping() || this.isPissing();
+    }
+
+    @Inject(method="tick()V", at=@At("HEAD"))
+    public void injectTick(CallbackInfo info) {
+        this.tryAddPissingStatus();
     }
 }
